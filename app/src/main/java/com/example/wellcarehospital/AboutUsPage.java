1f3e570a87c71c160cf2a0edc90a4f729467dfde
asmi <asmisutariya0702@gmail.com>
2023-10-02 00:21:23 +0530

    feed back page ui

--- app/src/main/java/com/example/wellcarehospital/AboutUsPage.java
+++ app/src/main/java/com/example/wellcarehospital/AboutUsPage.java
@@ -1,8 +1,11 @@
 package com.example.wellcarehospital;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class AboutUsPage extends AppCompatActivity {
 
@@ -10,5 +13,20 @@ public class AboutUsPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_us_page);
+    }
+    public void call_website(View v)
+    {
+        Intent i=new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.apollohospital.com"));
+        startActivity(i);
+    }
+    public void call_call(View v)
+    {
+        Intent i=new Intent(Intent.ACTION_VIEW, Uri.parse("tel:+8780476710"));
+        startActivity(i);
+    }
+    public void call_map(View v)
+    {
+        Intent i=new Intent(Intent.ACTION_VIEW, Uri.parse("22.47270213299797, 70.75427816645829"));
+        startActivity(i);
     }
 }
