5f65e250fd1a56809f660842afad9f95615508ef
dhruvi <138846379+dhruviwebito@users.noreply.github.com>
2023-10-02 21:48:15 +0530

    book patient

---
+++ app/src/main/java/com/example/wellcarehospital/book_patient.java
@@ -0,0 +1,14 @@
+package com.example.wellcarehospital;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class book_patient extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_book_patient);
+    }
+}
